6ad5af8eaa02b918894598c33de25f523255e326
Axel Ismirlian <aismirl@us.ibm.com>
2014-11-15 22:29:57 -0600

    Tank controls are fixed

--- Assets/Scripts/TankTop.cs
+++ Assets/Scripts/TankTop.cs
@@ -20,31 +20,27 @@ public class TankTop : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-				if (Input.GetButtonDown (shoot)) {
-						Fire ();
-				}
-				float input = 0;
-				if (Input.GetButton (xRotate)) {
-						input = -1;
-				} else if (Input.GetButton (yRotate)) {
-						input = 1;
-				
-				} else {
-						input = 0;
-				}
-						
-						transform.Rotate (Vector3.forward, 180f * input * Time.deltaTime);
-
-
-				}
+		if (Input.GetButtonDown (shoot)) {
+				Fire ();
+		}
+		float input = 0;
+		if (Input.GetButton (xRotate)) {
+				input = -1;
+		} else if (Input.GetButton (yRotate)) {
+				input = 1;
+		
+		} else {
+				input = 0;
+		}
+		
+		Debug.Log (xRotate + " " + yRotate + " " + input);				
+		transform.Rotate (Vector3.forward, 180f * input * Time.deltaTime);
+	}
 		
 	void Fire(){
 		if (CanFire) {
 			Debug.Log ("Position: " + transform.position + " Forward: " + transform.forward);
 			Rigidbody2D bullet = (Rigidbody2D)Instantiate (shot, transform.position + transform.up *.60f, Quaternion.Euler(0,0,0));
-//			bullet.transform.position.x += transform.forward.x *2;
-//			bullet.transform.position.y += transform.forward.y * 2;
-//			bullet.transform.position.z += transform.forward.z * 2;
 			bullet.rigidbody2D.velocity = transform.up * 10;
 			CanFire = false;
 			StartCoroutine(ShotsFired());
