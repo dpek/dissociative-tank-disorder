3ddf7343c1450dbb2b94ea29d74c47df935aa459
rosatamsen <drrt@cs.utexas.edu>
2014-11-16 13:11:09 -0600

    goals without respawn

---
+++ Assets/Scripts/GoalBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalBehaviour : MonoBehaviour {
+
+	public Team team;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if( collider.gameObject.tag == "Tank1" || collider.gameObject.tag == "Tank2"){
+			if(team == Team.TEAM2){
+				State.team1Score += 2;
+
+			}
+		}
+		else if( collider.gameObject.tag == "Tank3" || collider.gameObject.tag == "Tank4"){
+			if(team == Team.TEAM1){
+				State.team2Score += 2;
+
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
