e519e9d3aa427a21a90c62dc0386cd4df625c662
Axel Ismirlian <aismirl@us.ibm.com>
2014-11-16 10:06:00 -0600

    Fixed Bug with P1 input

--- Assets/Scripts/TankTop.cs
+++ Assets/Scripts/TankTop.cs
@@ -20,9 +20,9 @@ public class TankTop : MonoBehaviour {
 			Fire ();
 		}
 		float input = 0;
-		if (Input.GetButton (xRotate)) {
+		if (Input.GetButton (yRotate)) {
 			input = -1;
-		} else if (Input.GetButton (yRotate)) {
+		} else if (Input.GetButton (xRotate)) {
 			input = 1;
 		} else {
 			input = 0;
