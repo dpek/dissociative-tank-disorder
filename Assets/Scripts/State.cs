3fc7ddf00c5deb7a9ff2879ae9a11aaa27fb21f2
Darwin Pek <darwinpek@gmail.com>
2014-11-16 14:26:36 -0600

    Updated game end script

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -5,6 +5,8 @@ public class State : MonoBehaviour {
 	
 	public static int team1Score = 0;
 	public static int team2Score = 0;
+	public static int team1RoundScore = 0;
+	public static int team2RoundScore = 0;
 	public static float elapsedTime = 0;
 	public static float roundTime = 0;
 	public static int round = 1;
@@ -13,6 +15,8 @@ public class State : MonoBehaviour {
 	public static void reset(float roundTime) {
 		team1Score = 0;
 		team2Score = 0;
+		team1RoundScore = 0;
+		team2RoundScore = 0;
 		elapsedTime = 0;
 		State.roundTime = roundTime;
 	}
