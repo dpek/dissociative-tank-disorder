c02bb22efcd50bc8bf7ccfb814546022a6527b6b
rosatamsen <drrt@cs.utexas.edu>
2014-11-16 13:33:48 -0600

    lava kill and respawns

---
+++ Assets/Lava.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		if( collider.gameObject.tag == "Tank1" || collider.gameObject.tag == "Tank2"){
+				State.team1Score --;
+				collider.GetComponent<Tank>().Respawn(5);
+		}
+		else if( collider.gameObject.tag == "Tank3" || collider.gameObject.tag == "Tank4"){
+				State.team2Score--;
+				collider.GetComponent<Tank>().Respawn(5);
+	
+		}
+	}
+}
