f8e884b0516772b19c6d1fd849e37eb74e5eee96
rosatamsen <drrt@cs.utexas.edu>
2014-11-16 13:14:57 -0600

    steamroller respawn

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -5,29 +5,18 @@ public class GoalBehaviour : MonoBehaviour {
 
 	public Team team;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-
 	void OnTriggerEnter2D(Collider2D collider){
 		if( collider.gameObject.tag == "Tank1" || collider.gameObject.tag == "Tank2"){
 			if(team == Team.TEAM2){
 				State.team1Score += 2;
-
+				collider.GetComponent<Tank>().Respawn(0);
 			}
 		}
 		else if( collider.gameObject.tag == "Tank3" || collider.gameObject.tag == "Tank4"){
 			if(team == Team.TEAM1){
 				State.team2Score += 2;
-
+				collider.GetComponent<Tank>().Respawn(0);
 			}
 		}
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
