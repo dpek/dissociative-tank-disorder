f6c3ea2909b26b7979f3e63c5d42cc14e3ccd772
Darwin Pek <darwinpek@gmail.com>
2014-11-16 10:29:11 -0600

    Improved Menu Level UI

---
+++ Assets/Scripts/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuItem : MonoBehaviour {
+
+	public string level;
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel(level);
+	}
+}
